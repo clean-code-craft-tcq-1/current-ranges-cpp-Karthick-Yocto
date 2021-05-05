0e02fd27733cd54bc7bb7ad14ce67b5beb1d3dba
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-05 18:27:35 +0530

    Update current_range.h

--- current_range.h
+++ current_range.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <vector>
 
+using namespace std;
+
+struct RangeInfo
+{
+	int m_min;
+	int m_max;
+	int m_numsInRange;
+}; 
+
+
+void processRangeInfo(std::vector<int>, std::vector<RangeInfo*>&);
+
+void findRanges(const std::vector<int>&, std::vector<RangeInfo*>&);
+
+void displayRangeInfo(std::vector<RangeInfo*>&);
